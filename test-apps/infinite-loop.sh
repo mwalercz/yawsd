ff9e5d8d33053a3a62f9f78c4d2b17d1e7ab9262
mwalercz <mwalerczuk@gmail.com>
2017-12-01 01:30:38 +0100

    added test apps

---
+++ test-apps/infinite-loop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+i=1
+while :
+do
+	echo "$i"
+	i=${i}+1
+	sleep 3
+done
---
+++ test-apps/ten-sec-loop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#!/bin/bash
+for i in {1..10}
+do
+   echo "$i"
+   sleep 1
+done
